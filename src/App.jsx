0e1cb97941b2125d6e8786dea768accf605a47e5
Ajith <ajith@DESKTOP-ORLICE6.local>
2026-06-16 09:44:40 +0530

    Normalize line endings and add .gitattributes

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <nav className="top-nav">
+        <div className="nav-brand">Vite React</div>
+        <ul className="nav-links">
+          <li><a href="#home">pansss</a></li>
+          <li><a href="#features">sirt</a></li>
+          <li><a href="#docs">Dress</a></li>
+          <li><a href="#social">shoes</a></li>
+          <li><a href="#contact"></a></li>
+        </ul>
+      </nav>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
